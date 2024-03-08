ef6510968150cc877a5b19948533d4c1fc51932a
yilin <ylin3759@gmail.com>
2024-03-08 09:58:00 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="yololab",
-    version="1.0.0",
+    version="0.0.1",
     packages=find_packages(),
-    url="https://github.com/username_0/test",
-    license="MIT",
-    author="username_0",
-    author_email="<EMAIL>",
-    description="test",
+    url="https://github.com/mojiexiaozi/yololab.git",
+    license="Apache v2",
+    author="yilin",
+    author_email="ylin3759@gmail.com",
+    description="yolov8 lab",
 )
